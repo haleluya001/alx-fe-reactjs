28715a36a1222fa1ffbee1068a7632313e35b837
haleluya001 <haleluyatigabe1990@gmail.com>
2025-11-22 19:33:04 +0300

    first commit

---
+++ recipe-sharing-app/src/recipeStore.jsx
@@ -0,0 +1,8 @@
+// src/recipeStore.jsx
+import { create } from 'zustand';
+
+export const useRecipeStore = create((set) => ({
+  recipes: [],
+  addRecipe: (newRecipe) => set((state) => ({ recipes: [...state.recipes, newRecipe] })),
+  setRecipes: (recipes) => set({ recipes }),
+}));
