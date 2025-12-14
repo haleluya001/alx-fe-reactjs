3212b489a61e3adf2bfb62f6cf35ca41e1577e66
haleluya001 <haleluyatigabe1990@gmail.com>
2025-12-14 22:54:38 +0300

    Add advanced routing with BrowserRouter

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,4 +1,5 @@
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import Home from './pages/Home';
 import Profile from './pages/Profile';
 import ProfileDetails from './pages/ProfileDetails';
@@ -8,26 +9,25 @@ import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
 
-      {/* Protected Route */}
-      <Route
-        path="/profile"
-        element={
-          <ProtectedRoute>
-            <Profile />
-          </ProtectedRoute>
-        }
-      >
-        {/* Nested Routes */}
-        <Route path="details" element={<ProfileDetails />} />
-        <Route path="settings" element={<ProfileSettings />} />
-      </Route>
+        <Route
+          path="/profile"
+          element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          }
+        >
+          <Route path="details" element={<ProfileDetails />} />
+          <Route path="settings" element={<ProfileSettings />} />
+        </Route>
 
-      {/* Dynamic Route */}
-      <Route path="/blog/:id" element={<BlogPost />} />
-    </Routes>
+        <Route path="/blog/:id" element={<BlogPost />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
