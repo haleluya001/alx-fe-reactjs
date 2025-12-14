f612e8b3d98369a6007d8a3ae2bbc1f67e83193b
haleluya001 <haleluyatigabe1990@gmail.com>
2025-12-14 21:00:44 +0300

    first commit

---
+++ react-query-demo/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import PostsComponent from './components/PostsComponent';
+import { ReactQueryDevtools } from 'react-query/devtools';
+
+const queryClient = new QueryClient();
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <div>
+        <h1>React Query Demo: Posts</h1>
+        <PostsComponent />
+      </div>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
+
+export default App;
---
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+
+const fetchPosts = async () => {
+  const res = await axios.get('https://jsonplaceholder.typicode.com/posts');
+  return res.data;
+};
+
+const PostsComponent = () => {
+  const { data, isLoading, isError, error, refetch } = useQuery('posts', fetchPosts, {
+    staleTime: 1000 * 60, // 1 minute cache
+    cacheTime: 1000 * 60 * 5, // 5 minutes
+  });
+
+  if (isLoading) return <p>Loading posts...</p>;
+  if (isError) return <p>Error: {error.message}</p>;
+
+  return (
+    <div>
+      <button onClick={() => refetch()} style={{ marginBottom: '10px' }}>
+        Refetch Posts
+      </button>
+      <ul>
+        {data.map((post) => (
+          <li key={post.id}>
+            <strong>{post.title}</strong>
+            <p>{post.body}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default PostsComponent;
